04adeb21a6f6712201bc0958567648b4eeff930b
Alivefalcon <sanjayreddysdet@gmail.com>
2023-12-12 01:06:09 +0000

    Initial changes

---
+++ app/src/main/java/tees/ac/uk/mad/w9538022/recipeapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package tees.ac.uk.mad.w9538022.recipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tees.ac.uk.mad.w9538022.recipeapp.ui.theme.RecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecipeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
